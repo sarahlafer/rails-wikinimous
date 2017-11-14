a0be07940e37e54b433d54f8cb374a298fcdb059
Sarah Lafer <sarah_lafer@yahoo.com>
2017-11-14 16:16:21 +0100

    Wikinimous basics

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'articles/new', to: 'articles#new', as: :new_article
+
+  post 'articles', to: 'articles#create', as: :articles
+
+  get 'articles', to: 'articles#index'
+
+  get 'articles/:id', to: 'articles#show', as: :article
+
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+
+  patch 'articles/:id', to: 'articles#update'
+
+  delete 'articles/:id', to: 'articles#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
